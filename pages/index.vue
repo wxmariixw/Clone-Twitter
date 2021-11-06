a2dc89224923ea204c7d71377426d66aec500fd8
wxmariixw <marisftsabreu@gmail.com>
2021-11-06 11:20:57 -0300

    atualizações

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app" class="w-screen h-screen bg-gray-50">
+  <div id="app" class="w-screen bg-gray-50">
     <!--navegação-->
     <nav class="flex w-full h-16 bg-white border border-gray-100 rounded shadow-md">
-      <div class="flex content-center max-w-screen-xl h-16 m-auto">
+      <div class="flex w-full xl:w-11/12 h-16 m-auto">
         <!--botões desktop-->
         <div class="flex justify-between inline-block w-1/3 h-16">
           <button v-for="tab in tabs" class="w-auto my-2 px-2 text-base text-blue-400 font-semibold hover:bg-blue-50 hover:text-blue-700 rounded-full">
@@ -25,23 +25,37 @@
     </nav>
     <div class="flex content-center max-w-screen-xl justify-around m-auto my-5">
       <!--perfil-->
-      <div class="flex justify-around w-1/4 h-full border-2 border-gray-300 rounded-full bg-white shadow-md">
+      <div class="flex justify-around w-1/4 h-full border-2 border-gray-300 rounded-3xl bg-white shadow-md">
         <p>a</p>
       </div>
       <!--tweets section-->
-      <div class="flex justify-around w-2/5 h-full border-2 border-gray-300 rounded-full bg-white shadow-md">
+      <div class="grid grid-cols-1 divide-y divide-black divide-opacity-20 w-2/5 h-full border-2 border-gray-300 rounded-3xl bg-white shadow-md px-2">
         <!--cabeçalho-->
-        <div class="flex content-start ">
-        <p class="flex text-xl mt-2 ml-2">Tweets</p>
+        <div class="flex inline-block w-full justify-start">
+          <p class="flex inline-block text-xl mt-2 mb-4 ml-2 font-semibold text-gray-400">Tweets</p>
+        </div>
+        <!--tweet-->
+        <div v-for="tweet in tweets" class="flex w-full">
+          <div class="flex inline-block">
+            <img :src="`${tweet.src}`" alt="icones" class="flex inline-block w-14 h-14 m-1 rounded-full"/>
+          </div>
+          <div class="w-4/5 m-2">
+            <div class="flex h-14">
+              <p class="flex place-self-center inline-block mx-2">{{tweet.name}} </p>
+              <p class="flex place-self-center inline-block mx-2">{{tweet.user}} </p>
+            </div>
+            <div class="grid grid-rows-1 nline-block">
+              <p class="">{{tweet.text}} </p>
+            </div>
+          </div>
         </div>
       </div>
       <!--sugestions-->
-      <div class="flex justify-around w-1/4 h-full border-2 border-gray-300 rounded-full bg-white shadow-md">
+      <div class="flex justify-around w-1/4 h-full border-2 border-gray-300 rounded-3xl bg-white shadow-md">
         <p>a</p>
       </div>
     </div>
   </div>
-    
 </template>
 
 <script>
@@ -62,6 +76,16 @@ export default {
         {icone: 'comments', id: 'messages'},
         {icone: 'wrench', id: 'settings'},
         {icone: 'pen', id: 'edit'},
+      ],
+      tweets: [
+        {src: 'https://pbs.twimg.com/media/EcGdw6xXsAMkqGF.jpg',user: '@blackspider', name: 'Black Spider', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin sem purus, at blandit leo cursus tincidunt. Vestibulum vulputate nulla nisi, vel ullamcorper ex tempor sed. Integer nec quam in erat ullamcorper dictum. Morbi interdum porta ipsum, a vestibulum lacus vulputate non. Donec congue lorem non euismod lobortis. Donec et.'},
+        {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVPX7Fm7yD7MHd1RpTo8OMc1fEY9xP7HsgMuIvb8TX59Q8MeCyDZhgWb21zqrIAfGDheg&usqp=CAU',user: '@eletricshock', name: 'Eletric Shock', text: 'lorem ipsum'},
+        {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlnCXSeYCzYx2nY3dx65tLM5PISB9eO-GyZh-AKfC8m8-FfkDrpaESTi8B7ZCc7BKvlow&usqp=CAU',user: '@strogest', name: 'Strongest', text: 'lorem ipsum'},
+        {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7H_WYrpsbfP-KaeG1pMHKtOe9NulDKxVLUgztsFI5QdGxGheVpywIJnzP7G4Fu2k2lrw&usqp=CAU',user: '@coldfire', name: 'Cold Fire', text: 'lorem ipsum'},
+        {src: 'https://pbs.twimg.com/media/EcGdw6xXsAMkqGF.jpg',user: '@blackspider', name: 'Black Spider', text: 'lorem ipsum'},
+        {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVPX7Fm7yD7MHd1RpTo8OMc1fEY9xP7HsgMuIvb8TX59Q8MeCyDZhgWb21zqrIAfGDheg&usqp=CAU',user: '@eletricshock', name: 'Eletric Shock', text: 'lorem ipsum'},
+        {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlnCXSeYCzYx2nY3dx65tLM5PISB9eO-GyZh-AKfC8m8-FfkDrpaESTi8B7ZCc7BKvlow&usqp=CAU',user: '@strogest', name: 'Strongest', text: 'lorem ipsum'},
+        {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7H_WYrpsbfP-KaeG1pMHKtOe9NulDKxVLUgztsFI5QdGxGheVpywIJnzP7G4Fu2k2lrw&usqp=CAU',user: '@coldfire', name: 'Cold Fire', text: 'lorem ipsum'},
       ]
     }
   }
